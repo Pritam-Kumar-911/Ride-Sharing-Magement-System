851445fcdd44b2814cbf482bccb8440bcbe690bf
Pritam Kumar <pritam03128910155@gmail.com>
2025-07-01 15:08:08 +0500

    Setting up Panel

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("UniRide App");
+
+        JPanel mainPanel = new JPanel();
+        CardLayout cardLayout = new CardLayout();
+        mainPanel.setLayout(cardLayout);
+
+        frame.add(mainPanel);
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setBackground(Color.WHITE);
+        frame.setResizable(false);
+    }
+}
